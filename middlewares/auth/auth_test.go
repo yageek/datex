7f4ca478ee95933d33f454240b50b4a36c37c2d0
Yannick Heinrich <yannick.heinrich@gmail.com>
2014-11-27 12:27:25 +0100

    Add test

--- middlewares/auth/auth_test.go
+++ middlewares/auth/auth_test.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"net/http"
+	"strings"
 	"testing"
 	"time"
 )
@@ -10,7 +11,8 @@ func TestSignature(t *testing.T) {
 
 	user := &ApiUser{PrivateKey: "qwertz", PublicKey: "asdf"}
 
-	req, _ := http.NewRequest("POST", "http://localhost:3000/ellipsoid/create", nil)
+	body := `{"name": "test", "a" : 1 , "b" : 1}`
+	req, _ := http.NewRequest("POST", "http://localhost:3000/ellipsoid/create", strings.NewReader(body))
 	req.Header.Add("Content-Type", "application/json")
 
 	v := req.URL.Query()
@@ -23,5 +25,14 @@ func TestSignature(t *testing.T) {
 	req.URL.RawQuery = v.Encode()
 
 	client := &http.Client{}
-	client.Do(req)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		t.Errorf("Error:%s\n", err)
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Create failed :(\n")
+	}
 }
