9313b959013a0c6602a9032b445bdb9a052fa2d7
YaGeek <yannick.heinrich@gmail.com>
2014-11-28 22:06:42 +0100

    Add unit

--- handlers/unit/unit.go
+++ handlers/unit/unit.go
@@ -1,4 +1,4 @@
-package units
+package unit
 
 type UnitType int
 
@@ -11,9 +11,21 @@ const (
 	Angle
 )
 
-type Units struct {
+type Unit struct {
 	EPSG        int    `json:"epsg" bson:"epsg"`
 	Name        string `json:"name" bson:"name"`
 	Source      string `json:"source,omitempty" bson:"source,omitempty"`
 	Description string `json:"description,omitempty" bson:"description,omitempty"`
+}
+
+func (u *Unit) CollectionName() string {
+	return UnitCollectionName
+}
+
+func (u *Unit) ValidCreation() (bool, string) {
+	if u.EPSG == 0 || u.Name == "" {
+		return false, "EPSG and Name is mandatory"
+	} else {
+		return true, ""
+	}
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/codegangsta/negroni"
 	"github.com/go-gis/index-backend/handlers/ellipsoid"
+	"github.com/go-gis/index-backend/handlers/unit"
 	"github.com/go-gis/index-backend/middlewares"
 	"github.com/go-gis/index-backend/middlewares/auth"
 	"github.com/go-gis/index-backend/middlewares/mongo"
@@ -21,6 +22,9 @@ func main() {
 
 	router.HandleFunc("/ellipsoid/all", mongo.All(&ellipsoid.Ellipsoid{})).Methods("GET")
 	router.HandleFunc("/ellipsoid/create", auth.SecureHandleFunc(mongo.Create(&ellipsoid.Ellipsoid{}))).Methods("POST")
+
+	router.HandleFunc("/unit/all", mongo.All(&unit.Unit{})).Methods("GET")
+	router.HandleFunc("/unit/create", auth.SecureHandleFunc(mongo.Create(&unit.Unit{}))).Methods("POST")
 
 	n.UseHandler(router)
 
