c5887fe14d9e37e4cfaefd4639fa28a2ee6b054b
YaGeek <yannick.heinrich@gmail.com>
2014-11-30 10:45:03 +0100

    Remove unused code

--- handlers/ellipsoid/ellipsoid.go
+++ handlers/ellipsoid/ellipsoid.go
@@ -1,15 +1,6 @@
 //Manages handlers targeting ellipsoids
 package ellipsoid
 
-import (
-	"encoding/json"
-	. "github.com/go-gis/datex/middlewares"
-	"github.com/go-gis/datex/middlewares/mongo"
-	"gopkg.in/mgo.v2/bson"
-	"log"
-	"net/http"
-)
-
 const EllipsoidCollectionName = "ellipsoids"
 
 type Ellipsoid struct {
@@ -34,59 +25,4 @@ func (e *Ellipsoid) ValidCreation() (bool, string) {
 	}
 
 	return true, ""
-}
-
-// Returns all the ellipsoids presents in the database
-func AllEllipsoid(w http.ResponseWriter, r *http.Request) {
-	c := mongo.Collection(r, EllipsoidCollectionName)
-
-	var results []Ellipsoid
-	err := c.Find(bson.M{}).All(&results)
-
-	if err != nil {
-		http.Error(w, "Could not retrieve ellipse", http.StatusInternalServerError)
-		return
-	}
-
-	data, _ := json.Marshal(results)
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(data)
-
-}
-
-// Create an ellipsoid with the JSON provided in the request.
-// The JSON should provide least have a name, a semi-major-axis, a semi-minor-axis
-// or the inverse flattening. If the deprecated value is not provided, it is considered
-// as false.
-func CreateEllipsoid(w http.ResponseWriter, r *http.Request) {
-
-	e := Ellipsoid{}
-	data := GetData(r)
-
-	err := json.Unmarshal(data, &e)
-
-	if err != nil {
-		log.Printf("Error by decoding JSON:%s\n", err)
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-
-	if e.Name == "" || e.SemiMinorAxis == 0 || (e.SemiMinorAxis == 0 && e.InverseFlattening == 0) {
-		message := "Ellipsoid name, semi-major-axis, inverse-flattening or semi-minor-axis are mandatory"
-		log.Println(message)
-		http.Error(w, message, http.StatusBadRequest)
-		return
-	}
-
-	c := mongo.Collection(r, EllipsoidCollectionName)
-
-	err = c.Insert(e)
-
-	if err != nil {
-		log.Printf("Error by inserting data: %s\n", err)
-		http.Error(w, "DB error :(", http.StatusInternalServerError)
-	}
-
-	w.WriteHeader(http.StatusOK)
 }
