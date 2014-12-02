be85f6973a59ecbda9a31ffe9caadc3db6109a80
Yannick Heinrich <yannick.heinrich@gmail.com>
2014-12-02 17:11:04 +0100

    Add Unit type and time

--- handlers/unit/unit.go
+++ handlers/unit/unit.go
@@ -1,5 +1,9 @@
 package unit
 
+import (
+	"time"
+)
+
 type UnitType int
 
 const (
@@ -11,12 +15,29 @@ const (
 	Angle
 )
 
+func (t UnitType) String() string {
+	switch t {
+	case Time:
+		return "time"
+	case Length:
+		return "length"
+	case Scale:
+		return "scale"
+	case Angle:
+		return "angle"
+	default:
+		return "unknown"
+	}
+}
+
 type Unit struct {
-	EPSG        int    `json:"epsg" bson:"epsg"`
-	Name        string `json:"name" bson:"name"`
-	Deprecated  bool   `json:"deprecated" bson:"deprecated"`
-	Source      string `json:"source,omitempty" bson:"source,omitempty"`
-	Description string `json:"description,omitempty" bson:"description,omitempty"`
+	EPSG         int       `json:"epsg" bson:"epsg"`
+	Name         string    `json:"name" bson:"name"`
+	Deprecated   bool      `json:"deprecated" bson:"deprecated"`
+	Source       string    `json:"source,omitempty" bson:"source,omitempty"`
+	Description  string    `json:"description,omitempty" bson:"description,omitempty"`
+	Type         UnitType  `json:"type" bson:"type"`
+	RevisionDate time.Time `json:"revision_date,omitempty" bson:"type,omitempty"`
 }
 
 func (u *Unit) CollectionName() string {
