9afe70dfe29aaf08a4ed8b3394068efba0d0106b
Yannick Heinrich <yannick.heinrich@gmail.com>
2014-12-08 10:04:21 +0100

    Fill validcreation method for the datum handler

--- handlers/datum/datum.go
+++ handlers/datum/datum.go
@@ -15,21 +15,24 @@ const (
 )
 
 type Datum struct {
-	Name          string              `json:"name" bson:"name"`
-	Deprecated    bool                `json:"deprecated" bson:"deprecated"`
-	Ellipsoid     ellipsoid.Ellipsoid `json:"ellipsoid" bson:"ellipsoid"`
-	Type          DatumType           `json:"type" bson:"type"`
-	PrimeMeridian meridian.Meridian   `json:"prime_meridian" bson:"prime_meridian"`
-	EPSG          int                 `json:"epsg,omitempty" bson:"epsg,omitempty"`
-	Epoch         int                 `json:"epoch,omitempty" bson:"epoch,omitempty"`
-	Description   string              `json:"description,omitempty" bson:"description,omitempty"`
-	Source        string              `json:"source,omitempty" bson:"source,omitempty"`
+	Name          string               `json:"name" bson:"name"`
+	Deprecated    bool                 `json:"deprecated" bson:"deprecated"`
+	Ellipsoid     *ellipsoid.Ellipsoid `json:"ellipsoid" bson:"ellipsoid"`
+	Type          DatumType            `json:"type" bson:"type"`
+	PrimeMeridian *meridian.Meridian   `json:"prime_meridian" bson:"prime_meridian"`
+	EPSG          int                  `json:"epsg,omitempty" bson:"epsg,omitempty"`
+	Epoch         int                  `json:"epoch,omitempty" bson:"epoch,omitempty"`
+	Description   string               `json:"description,omitempty" bson:"description,omitempty"`
+	Source        string               `json:"source,omitempty" bson:"source,omitempty"`
 }
 
 func (d *Datum) CollectionName() string {
 	return DatumCollectionName
 }
 
-func (m *Datum) ValidCreation() (bool, string) {
+func (d *Datum) ValidCreation() (bool, string) {
+	if d.Name == "" || d.Ellipsoid == nil || d.PrimeMeridian == nil {
+		return false, "Name, Ellipsoid and Prime meridian are mandatory"
+	}
 	return true, ""
 }
