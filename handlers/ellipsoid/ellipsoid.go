92b974d5905d4f4ae35a42200d8ffe3f23a90b24
YaGeek <yannick.heinrich@gmail.com>
2014-12-13 12:32:00 +0100

    Use the same fields for son and database

--- handlers/ellipsoid/ellipsoid.go
+++ handlers/ellipsoid/ellipsoid.go
@@ -10,13 +10,13 @@ const EllipsoidCollectionName = "ellipsoids"
 type Ellipsoid struct {
 	Name              string    `json:"name" bson:"name"`
 	EPSG              int       `json:"epsg" bson:"epsg"`
-	RevisionDate      time.Time `json:"revision_date,omitempty" bson:"type,omitempty"`
+	RevisionDate      time.Time `json:"revision_date,omitempty" bson:"revision_date,omitempty"`
 	Deprecated        bool      `json:"deprecated" bson:"deprecated"`
 	Source            string    `json:"source,omitempty" bson:"source,omitempty"`
 	Description       string    `json:"description,omitempty" bson:"description,omitempty"`
-	SemiMajorAxis     float64   `json:"a"  bson:"semi-major-axis"`
-	SemiMinorAxis     float64   `json:"b,omitempty" bson:"semi-minor-axis,omitempty"`
-	InverseFlattening float64   `json:"inv_f,omitempty"  bson:"inverse-flattening,omitempty"`
+	SemiMajorAxis     float64   `json:"semi-major-axis"  bson:"semi-major-axis"`
+	SemiMinorAxis     float64   `json:"semi-minor-axis,omitempty" bson:"semi-minor-axis,omitempty"`
+	InverseFlattening float64   `json:"inverse-flattening,omitempty"  bson:"inverse-flattening,omitempty"`
 }
 
 func (e *Ellipsoid) CollectionName() string {
